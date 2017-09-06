32b44f0d382351884412b32843a4917baf20b4d8
castcoco <castcoco@hotmail.com>
2017-09-06 18:36:12 +0100

    2nd? commit?

--- mainregister.js
+++ mainregister.js
@@ -1,105 +1,102 @@
 $(document).ready(function(){
   checkLoggedin();
   register();
+  signin();
+  //validation();
   $('.logoutbtn').click(logout);
 });
 
 function register(){
-$('#registerbtn').click(function(e){
-e.preventDefault();
-  console.log('register');
+  $('#registerbtn').click(function(e){
+    e.preventDefault();
+    console.log('register');
     var username = $('#signUser').val();
     var email = $('#signEmail').val();
     var password = $('#signPwd').val();
     if (username == "" || password == "" || email == "" ){
-      $('div#singuperror').text("Please fill in all the fields");
+      $('div#singuperror').html("Please fill in all the fields").css('color','red');
     }else{
-      //serialise the data
-      $.post( $('.modalSignUp').attr('action'),
-      $('.modalSignUp :input').serializeArray(),
-      function(info){
-      //call back 
-      $('div#singuperror').empty();
-      $('div#signuperror').html(info);
-      clear();
-      }); 
-    }
-
-    $('.modalSignUp').submit( function(){
-      return false;
-    });
-});
-}		
+        //serialise the data
+        $.post( $('.modalSignUp').attr('action'),
+        $('.modalSignUp :input').serializeArray(),
+        function(info){
+        //call back 
+        $('div#singuperror').empty();
+        $('div#signuperror').html('Fail!');
+        clear();
+        }); 
+      }
+      $('.modalSignUp').submit(function(){
+        return false;
+      });
+  })
+};		
 
 function clear(){
-	$('.modalSignUp :input').each(function(){
-		$(this).val('');
-	});
+	$('.modalSignUp :input').val('');
+  $('div#signuperror').empty();
 }
-
 
 //signin
 function signin(){
-$('#loginbtn').submit(function(){
-	var user = $('#username').val();
-	var pass = $('#password').val();
-  if ( user =='' || ( pass == '')){
-  	console.log(user, pass);
-  	$('#loginerror').html("Please fill in all the fields");
-  }
-  else{
-    $.post( $('.modalLogin').attr('action'),
-    $('.modalLogin :input').serializeArray(),      
-    function(info){
-      //call back 
-      $('div#loginerror').html('error!');
-    }); 
+  $('#loginbtn').submit(function(){
+    var user = $('#username').val();
+    var pass = $('#password').val();
+    if ( user =='' || ( pass == '')){
+    	console.log(user, pass);
+    	$('#loginerror').html("Please fill in all the fields");
+    }
+    else{
+      $.post( $('.modalLogin').attr('action'),
+      $('.modalLogin :input').serializeArray(),      
+      function(info){
 
-  $('.modalLogin').submit(function(){
-    return false;
+        //call back 
+        $('.modal-content #loginerror').html('Login fail!');
+      }); 
+
+    $('.modalLogin').submit(function(){
+      return false;
+    });
+   }
   });
- }
-});
 }
-
-function remember(){
-  var rememberme = $.cookie('remember');
-  if (remember = 'true')
-  {
-    var username = $.cookie('username');
-    $('#username') = val(username);
-  }
-
-  if($('#remember').is(':checked')){
-    $.cookie('username', username,{expires:14});
-  }
-  else{
-    $.cookie('username', null);
-  }
-};
 
 
 function checkLoggedin(){
   var loggedin = getCookie('loggedin');
-
+  // hide login button
   console.log(loggedin);
   if(loggedin == 'true'){
     $('html').addClass('loggedin');
     console.log($('html').addClass('loggedin'));
     $('.loginHide').hide();
+    //enable the link for registered users
+    $("li .photohide").removeClass("disabled").find("a").removeAttr("onclick");
+    //enable save button and myrecord button in harvestdiary 
+    document.getElementById('saveBtn').disabled=false;
+    document.getElementById('myRecord').disabled=false;
   }else{
+    //hide logout button
     $('html').removeClass('loggedin');
     $('.logoutHide').hide();
-    $('nav .gallery').hide();
+
+    //disable the link for un-registered users
+    $("li .photohide").addClass("disabled").find("a").attr("onclick", "return false;");
+    //disable save button and myrecord button if the user isn't logged in
+     document.getElementById('saveBtn').disabled=true;
+     document.getElementById('myRecord').disabled=true;
   }
 };
+
 
 
 function logout(){
   alert("You've successfully logged out!");
   }
 
-// cookies from stawflow :https://stackoverflow.com/a/22852843/8338548
+//to save the cookies
+// cookies function extracted from stawflow :https://stackoverflow.com/a/22852843/8338548
 function getCookie(c_name) {
     var c_value = " " + document.cookie;
     var c_start = c_value.indexOf(" " + c_name + "=");
@@ -116,4 +113,3 @@ function getCookie(c_name) {
     }
     return c_value;
 }
-
